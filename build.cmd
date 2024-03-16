1424e0cc3b953130682512244ef0b14479f05ac1
filou56 <filouwelle@gmail.com>
2024-03-16 19:58:30 +0100

    Windows build update

--- build.cmd
+++ build.cmd
@@ -2,5 +2,6 @@ copy ..\elzwelle_sheet.py .
 mkdir dist
 mkdir dist\elzwelle_sheet
 \opt\miniconda3\Scripts\pyinstaller.exe elzwelle_sheet.py
-copy \opt\miniconda3\Library\bin\libcrypto-3-x64.dll dist\elzwelle_sheet\_internal
-copy \opt\miniconda3\Library\bin\libssh2.dll dist\elzwelle_sheet\_internal
+copy \opt\miniconda3\Library\bin\libcrypto-3-x64.dll 	dist\elzwelle_sheet\_internal
+copy \opt\miniconda3\Library\bin\libssh2.dll 			dist\elzwelle_sheet\_internal
+copy \opt\miniconda3\Library\bin\libssl-3-x64.dll		dist\elzwelle_sheet\_internal
